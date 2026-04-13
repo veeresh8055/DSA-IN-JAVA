1e3b9cac68cf9c4cd8d57f8bbad0dedffdc7b6f9
veeresh8055 <visesync@gmail.com>
2026-04-13 09:54:37 +0530

    Linked List

---
+++ 07-LinkedList/LL.java
@@ -0,0 +1,69 @@
+class LL {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // addFirst Data
+    public void addFirst(String data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+
+        }
+        newNode.next = head;
+        head = newNode;
+
+    }
+
+    // addLast data
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // print List
+    public void print() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            System.out.print(currNode.data + " --> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.addFirst("hii");
+        list.addFirst("hello");
+        list.addLast("Veeresh");
+        list.print();
+
+    }
+
+}
